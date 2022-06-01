172524bd4b48b958557aec8165f3f15e527dca80
eh250 <eh250@st-andrews.ac.uk>
2022-06-01 17:09:37 +0100

    Added get_horn_buff and get_morale_buff methods to unit_card class. Added a horn_card class for units which have a commander's horn effect.

--- gwent/cards/unit/unit_card.py
+++ gwent/cards/unit/unit_card.py
@@ -1,4 +1,6 @@
 from gwent.cards.card import Card
+from gwent.cards.unit.horn_card import HornCard
+
 
 class UnitCard(Card):
 
@@ -6,6 +8,7 @@ class UnitCard(Card):
         super().__init__(self, name, faction, row, strength)
         self.hero = hero
         self.agile = agile
+        self.morale_boost = False
 
     def get_row(self):
         if self.agile:
@@ -23,4 +26,22 @@ class UnitCard(Card):
 
     def get_strength_modifier(self, board):
         # check for morale boosts and horns
+
         return 1
+
+    def get_horn_buff(self, board):
+        horn = 1
+
+        for card in board.rows[self.row]:
+            if isinstance(card, HornCard):
+                horn *= 2
+
+        return horn
+
+    def get_morale_buff(self, board):
+        morale = -1 if self.morale_boost else 0
+
+        for card in board.rows[self.row]:
+            if isinstance(card, UnitCard) and card.morale_boost:
+                morale += 1
+        return morale

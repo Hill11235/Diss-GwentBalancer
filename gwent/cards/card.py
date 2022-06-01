72f469f4f3e0323b054a0ede46f099b0656d8947
eh250 <eh250@st-andrews.ac.uk>
2022-06-01 15:14:28 +0100

    Add docstring to card class, need to add methods when complete

--- gwent/cards/card.py
+++ gwent/cards/card.py
@@ -1,4 +1,28 @@
 class Card:
+    """
+    Class used to represent an individual card in the game.
+
+    Attributes
+    __________
+
+    name : String
+        name of a given card
+    faction : String
+        name of a given card's faction, empty indicates neutral card
+    row : int
+        the row that this card can be added to.
+    strength : int
+        the card's strength
+    hero : boolean
+        indicates whether the card is a hero card or not
+    horn : boolean
+        indicates whether the card has a commander's horn effect
+
+    Methods
+    _______
+
+    TODO add methods once complete
+    """
 
     def __init__(self, name, faction, row, strength):
         self.name = name

2dba4995b70938f51f750e6fa72f228ee50b1093
eh250 <eh250@st-andrews.ac.uk>
2022-06-01 15:23:51 +0100

    Added pass, reset, and lose methods to the player class. Added initial shell for the get_player_data method.

--- gwent/player.py
+++ gwent/player.py
@@ -14,9 +14,21 @@ class Player:
         self.hand = []
 
     # TODO general getter methods
-    # TODO pass method and reset passed method
-    # TODO lose method, decrement lives
     # TODO think about how to add initial decks
     # TODO mulligan method
     # TODO draw card method
     # TODO get JSON data method
+
+    def pass_round(self):
+        self.passed = True
+
+    def reset_round(self):
+        self.passed = False
+
+    def lose_round(self):
+        self.lives -= 1
+        return self.lives
+
+    def get_player_data(self):
+        # get name, lives, hand size, faction, passed etc as JSON/dictionary
+        pass

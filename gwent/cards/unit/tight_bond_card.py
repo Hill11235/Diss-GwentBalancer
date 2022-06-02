45b882c2dbb7c7e94777345d77120d4dab4688af
eh250 <eh250@st-andrews.ac.uk>
2022-06-02 14:40:49 +0100

    Completed TightBondCard method and class. Override get_active_strength method and have it check for any other cards with the same name in the the same row, buff strength accordingly.

--- gwent/cards/unit/tight_bond_card.py
+++ gwent/cards/unit/tight_bond_card.py
@@ -1,7 +1,19 @@
 from gwent.cards.unit.unit_card import UnitCard
+from gwent.cards.special.weather_card import WeatherCard
 
 
 class TightBondCard(UnitCard):
 
-    def __init__(self, name, faction, row, strength, hero, agile):
-        super().__init__(name, faction, row, strength, hero, agile)
+    def get_active_strength(self, board):
+        num_comrades = 0
+        horn_buff = self.get_horn_buff(board)
+        morale_buff = self.get_morale_buff(board)
+
+        for card in board.rows[self.row]:
+            if card.name == self.name:
+                num_comrades += 1
+
+        if any(isinstance(card, WeatherCard) for card in board.rows[self.row]):
+            return (num_comrades + morale_buff) * horn_buff
+        else:
+            return ((self.strength * num_comrades) + morale_buff) * horn_buff

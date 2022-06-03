0b97063f0db82a4e5bd166c009ca74cbd0e55dcf
eh250 <eh250@st-andrews.ac.uk>
2022-06-03 11:25:16 +0100

    Added ClearWeatherCard class and added place_card and battlecry methods for it.

---
+++ gwent/cards/special/clear_weather_card.py
@@ -0,0 +1,15 @@
+from gwent.cards.special.special_card import SpecialCard
+from gwent.cards.special.weather_card import WeatherCard
+
+
+class ClearWeatherCard(SpecialCard):
+
+    def place_card(self, board, player, opponent, opponent_board, target):
+        self.battlecry(board, player, opponent, opponent_board, target)
+
+    def battlecry(self, board, player, opponent, opponent_board, target):
+        for b in [board, opponent_board]:
+            for row in b.rows:
+                for card in row:
+                    if isinstance(card, WeatherCard):
+                        card.destroy(b, b.player)

3843d22ceeed166823e9816069abb054f11c1e55
eh250 <eh250@st-andrews.ac.uk>
2022-06-09 11:02:35 +0100

    Changed MusterCard battlecry method so that a player's deck is not modified in a loop.

--- gwent/cards/unit/muster_card.py
+++ gwent/cards/unit/muster_card.py
@@ -4,8 +4,8 @@ from gwent.cards.unit.unit_card import UnitCard
 class MusterCard(UnitCard):
 
     def battlecry(self, board, player, opponent, opponent_board, row, target):
-        # TODO check that modifying a list while iterating through it will not cause problems
+
         for card in player.deck:
             if card.name == self.name:
-                player.deck.remove(card)
                 card.place_card(board, player, opponent, opponent_board, row, target)
+        player.deck = [card for card in player.deck if not card.name == self.name]

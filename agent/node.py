148272d1fe4d33927800e648cb5244ca2667847f
eh250 <eh250@st-andrews.ac.uk>
2022-06-17 11:14:38 +0100

    Added some method shells to the node class.

--- agent/node.py
+++ agent/node.py
@@ -6,3 +6,16 @@ class Node:
         # number of wins this node has
         # upper confidence bound 1 of node
         pass
+
+    def is_terminal(self):
+        return len(self.get_all_children()) == 0
+
+    def get_all_children(self):
+        children = []
+        # get list of all children
+        return children
+
+    def get_random_child(self):
+        # return a random child node
+        pass
+

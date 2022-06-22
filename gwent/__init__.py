76b9145cfafda130b0a4b703b17c683d519b970f
eh250 <eh250@st-andrews.ac.uk>
2022-06-22 11:03:10 +0100

    updated init.py in Gwent directory to optimise imports

---
+++ gwent/__init__.py
@@ -0,0 +1,4 @@
+from .board import Board
+from .deck import Deck
+from .game import GameState
+from .player import Player
--- tests/gwent/game_testing.py
+++ tests/gwent/game_testing.py
@@ -1,11 +1,21 @@
 import unittest
+from gwent import *
 
 
 class GameTest(unittest.TestCase):
 
     def setUp(self):
-        # decks
-        # players
+        test_decks = "data/test_decks.csv"
+        file_name = "data/card_data.csv"
+        faction = "Nilfgaardian"
+        size = 22
+        seed = 123
+
+        self.deck = Deck(file_name, faction, size, seed)
+        self.monster, self.nilf, self.northern, self.scoiatael = self.deck.create_deck_using_list(test_decks)
+
+        self.player1 = Player("p1", "Nilfgaardian", self.nilf)
+        self.player2 = Player("p2", "Monster", self.monster)
         # boards
         # game
         pass

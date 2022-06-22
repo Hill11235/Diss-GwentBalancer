2bb152db7deade64bbbd204d838793b5a2acc400
eh250 <eh250@st-andrews.ac.uk>
2022-06-22 17:22:37 +0100

    Added initial test shells for the unit card unit tests

---
+++ tests/gwent/cards/unit/test_unit_card.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+
+class TestUnitCard(TestCase):
+
+    def setUp(self):
+        # unit card
+        # horn card
+        # morale card
+        # player
+        # board
+        pass
+
+    def test_get_row(self):
+        self.fail()
+
+    def test_place_card(self):
+        self.fail()
+
+    def test_battlecry(self):
+        self.fail()
+
+    def test_get_active_strength(self):
+        self.fail()
+
+    def test_get_horn_buff(self):
+        self.fail()
+
+    def test_get_morale_buff(self):
+        self.fail()
+
+    def test_get_current_status(self):
+        self.fail()

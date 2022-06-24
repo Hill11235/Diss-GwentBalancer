34a5319419d5156ad9c2bf1c0c1cfcc5f67e812e
eh250 <eh250@st-andrews.ac.uk>
2022-06-24 15:53:35 +0100

    Refactored muster cards battlecry so there isn't infinite recursion. Also added hand check in addition to deck check. Added unit tests for muster cards and checked a number of scenarios. Added basic unit tests for morale_boost cards.

--- gwent/cards/unit/muster_card.py
+++ gwent/cards/unit/muster_card.py
@@ -4,8 +4,18 @@ from gwent.cards.unit.unit_card import UnitCard
 class MusterCard(UnitCard):
 
     def battlecry(self, board, player, opponent, opponent_board, row, target):
+        explored = [self.card_id]
 
         for card in player.deck:
-            if card.name == self.name:
-                card.place_card(board, player, opponent, opponent_board, row, target)
+            if card.name == self.name and card.card_id not in explored:
+                explored.append(card.card_id)
+                card.place_card(board, player, opponent, opponent_board, card.get_row()[0], target)
+
         player.deck = [card for card in player.deck if not card.name == self.name]
+
+        for card in player.hand:
+            if card.name == self.name and card.card_id not in explored:
+                explored.append(card.card_id)
+                card.place_card(board, player, opponent, opponent_board, card.get_row()[0], target)
+
+        player.hand = [card for card in player.hand if not card.name == self.name]
---
+++ tests/gwent/cards/unit/test_muster_card.py
@@ -0,0 +1,80 @@
+from unittest import TestCase
+from gwent.cards import *
+from gwent import *
+
+
+class TestMusterCard(TestCase):
+
+    def setUp(self):
+        self.must1 = MusterCard("1", "dummy1", "Monster", 0, 8, False, False)
+        self.must2 = MusterCard("2", "dummy1", "Monster", 0, 10, False, False)
+        self.must3 = MusterCard("3", "dummy1", "Monster", 1, 7, False, False)
+        self.must4 = MusterCard("4", "dummy1", "Monster", 1, 9, False, False)
+        self.med1 = MedicCard("4", "med1", "Northern", 1, 4, False, False)
+
+        test_decks = "data/test_decks.csv"
+        file_name = "data/card_data.csv"
+        faction = "Nilfgaardian"
+        size = 22
+        seed = 123
+
+        self.deck = Deck(file_name, faction, size, seed)
+        monster, nilf, northern, scoiatael = self.deck.create_deck_using_list(test_decks)
+        self.player1 = Player("p1", "Nilfgaardian", nilf)
+        self.board1 = Board(self.player1)
+
+    def test_battlecry(self):
+        # checks that all instances of a muster card are played from hand and deck
+        self.player1.hand.append(self.must1)
+        self.player1.hand.append(self.must2)
+        self.player1.hand.append(self.must4)
+        self.player1.deck.append(self.must3)
+
+        # checks that all instances have been played to the correct rows and have been removed from the hand and deck.
+        self.must1.place_card(self.board1, self.player1, None, None, self.must1.row, None)
+        self.assertTrue(self.board1.rows[0].__contains__(self.must1))
+        self.assertTrue(self.board1.rows[0].__contains__(self.must2))
+        self.assertTrue(self.board1.rows[1].__contains__(self.must3))
+        self.assertTrue(self.board1.rows[1].__contains__(self.must4))
+        self.assertFalse(self.player1.hand.__contains__(self.must1))
+        self.assertFalse(self.player1.hand.__contains__(self.must2))
+        self.assertFalse(self.player1.deck.__contains__(self.must3))
+        self.assertFalse(self.player1.hand.__contains__(self.must4))
+        self.assertEqual(len(self.board1.rows[0]), 2)
+        self.assertEqual(len(self.board1.rows[1]), 2)
+
+    def test_no_graveyard(self):
+        # test that cards are not summoned from graveyard
+        self.player1.hand.append(self.must1)
+        self.player1.graveyard.append(self.must2)
+        self.player1.graveyard.append(self.must4)
+        self.player1.graveyard.append(self.must3)
+
+        self.must1.place_card(self.board1, self.player1, None, None, self.must1.row, None)
+        self.assertTrue(self.board1.rows[0].__contains__(self.must1))
+        self.assertTrue(self.player1.graveyard.__contains__(self.must2))
+        self.assertTrue(self.player1.graveyard.__contains__(self.must3))
+        self.assertTrue(self.player1.graveyard.__contains__(self.must4))
+        self.assertEqual(len(self.board1.rows[0]), 1)
+        self.assertEqual(len(self.board1.rows[1]), 0)
+
+    def test_medic_summon(self):
+        # tests that muster happens when medic summons card
+        self.player1.hand.append(self.med1)
+        self.player1.graveyard.append(self.must1)
+        self.player1.hand.append(self.must2)
+        self.player1.hand.append(self.must4)
+        self.player1.deck.append(self.must3)
+
+        self.med1.place_card(self.board1, self.player1, None, None, self.med1.row, self.must1)
+        self.assertTrue(self.board1.rows[0].__contains__(self.must1))
+        self.assertTrue(self.board1.rows[0].__contains__(self.must2))
+        self.assertTrue(self.board1.rows[1].__contains__(self.must3))
+        self.assertTrue(self.board1.rows[1].__contains__(self.must4))
+        self.assertTrue(self.board1.rows[1].__contains__(self.med1))
+        self.assertFalse(self.player1.graveyard.__contains__(self.must1))
+        self.assertFalse(self.player1.hand.__contains__(self.must2))
+        self.assertFalse(self.player1.deck.__contains__(self.must3))
+        self.assertFalse(self.player1.hand.__contains__(self.must4))
+        self.assertEqual(len(self.board1.rows[0]), 2)
+        self.assertEqual(len(self.board1.rows[1]), 3)

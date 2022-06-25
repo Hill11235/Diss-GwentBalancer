124c81cf919e0f228e168c2d12c392d147315346
eh250 <eh250@st-andrews.ac.uk>
2022-06-25 13:42:21 +0100

    Added unit tests for tight bond cards. Check that strength scales properly.

---
+++ tests/gwent/cards/unit/test_tight_bond_card.py
@@ -0,0 +1,51 @@
+from unittest import TestCase
+from gwent import *
+from gwent.cards import *
+
+
+class TestTightBondCard(TestCase):
+
+    def setUp(self):
+        self.tight1 = TightBondCard("1", "dummy1", "Monster", 0, 10, False, False)
+        self.tight2 = TightBondCard("2", "dummy1", "Monster", 0, 10, False, False)
+        self.tight3 = TightBondCard("3", "dummy1", "Monster", 0, 10, False, False)
+
+        test_decks = "data/test_decks.csv"
+        file_name = "data/card_data.csv"
+        faction = "Nilfgaardian"
+        size = 22
+        seed = 123
+
+        self.deck = Deck(file_name, faction, size, seed)
+        monster, nilf, northern, scoiatael = self.deck.create_deck_using_list(test_decks)
+        self.player1 = Player("p1", "Nilfgaardian", nilf)
+        self.board1 = Board(self.player1)
+
+    def test_get_active_strength(self):
+        self.board1.rows[0].append(self.tight1)
+        self.assertEqual(self.tight1.get_active_strength(self.board1), 10)
+
+        self.board1.rows[0].append(self.tight2)
+        self.assertEqual(self.tight1.get_active_strength(self.board1), 20)
+        self.assertEqual(self.tight2.get_active_strength(self.board1), 20)
+
+        self.board1.rows[0].append(self.tight3)
+        self.assertEqual(self.tight1.get_active_strength(self.board1), 30)
+        self.assertEqual(self.tight2.get_active_strength(self.board1), 30)
+        self.assertEqual(self.tight3.get_active_strength(self.board1), 30)
+
+    def test_horn_and_morale(self):
+        horn = HornUnitCard("4", "dummy2", "Monster", 0, 5, False, False)
+        morale = MoraleBoostCard("5", "dummy3", "Monster", 0, 6, False, False)
+
+        self.board1.rows[0].append(self.tight1)
+        self.board1.rows[0].append(self.tight2)
+        self.board1.rows[0].append(self.tight3)
+        self.board1.rows[0].append(horn)
+        self.board1.rows[0].append(morale)
+
+        self.assertEqual(self.tight1.get_active_strength(self.board1), 62)
+        self.assertEqual(self.tight2.get_active_strength(self.board1), 62)
+        self.assertEqual(self.tight3.get_active_strength(self.board1), 62)
+        self.assertEqual(horn.get_active_strength(self.board1), 6)
+        self.assertEqual(morale.get_active_strength(self.board1), 12)
